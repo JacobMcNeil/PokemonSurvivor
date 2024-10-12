158ff5f2eb71927154aa0214777541861d720aa4
Alex Steinkamp <jacob.mcneil8@gmail.com>
2024-10-11 21:37:03 -0500

    add

--- Build/newWebGL.framework.js
+++ Build/newWebGL.framework.js
@@ -2078,13 +2078,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4079376: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4079437: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4079501: function() {return Module.webglContextAttributes.powerPreference;},  
- 4079559: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4079614: function($0) {performance.now = function() { return $0; };},  
- 4079662: function($0) {performance.now = function() { return $0; };},  
- 4079710: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4079440: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4079501: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4079565: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4079623: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4079678: function($0) {performance.now = function() { return $0; };},  
+ 4079726: function($0) {performance.now = function() { return $0; };},  
+ 4079774: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
